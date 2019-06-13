ad022b6d92e675119a4aa1b87c26fda0320b7668
Vseelbach <51764374+Vseelbach@users.noreply.github.com>
2019-06-13 13:59:45 -0400

    p

---
+++ forStatement.java
@@ -0,0 +1,10 @@
+package ForLoop;
+https://github.com/Vseelbach/Spring-2019-First-class
+public class forStatement {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
